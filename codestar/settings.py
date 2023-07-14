0acfb8bb9b3e3841a9617687ff47c3c699ca0c5f
Anna Konstantynova <hannakonstantinova@gmail.com>
2023-07-14 15:27:22 +0000

    Make adjustments to deploy

--- codestar/settings.py
+++ codestar/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['my-django-blog-codestar.herokuapp.com', 'localhost']
+ALLOWED_HOSTS = ['my-django-blog-codestar-7068843747dc.herokuapp.com', 'localhost']
 
 
 # Application definition
